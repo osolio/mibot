35383f6e31ea846bf68d8eb4393421d62665510b
osolio <102782754+osolio@users.noreply.github.com>
2022-04-06 15:56:05 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -542,7 +542,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5289637475:AAFdCLoE7ywnesB31CJLZAafyrb9otgktQM'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5289637475:AAFdCLoE7ywnesB31CJLZAafyrb9otgktQM'
+    bot_token = '5187853556:AAGU_edCSJBUppTBJr5Wef3vlWdxNymXl4k'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
